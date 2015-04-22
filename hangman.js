1d41c15c5e6df2d8aec9f86484c26244c3e7f66a
Chris Davis <compsciguy777@gmail.com>
2015-04-22 16:35:16 -0500

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,3 +1,11 @@
-var canvas = document.getElementById('my_canvas')
-var context = canvas.getcontext("2d")
-var stage = createjs.
+var canvas = document.getElementById('my_canvas');
+var context = canvas.getcontext("2d");
+var stage = createjs.stage(canvas);
+
+var circle = new.createjs.shape();
+circle.graphics.beginfill("blue").draw(0,0,40);
+circle.x = circle.y = 50;
+
+stage.addchild(circle);
+
+stage.update();
